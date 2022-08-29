04320c46c3adca1cee6b1947b5081b14282dedc9
DevWonny <cjfdnjs1994@naver.com>
2022-08-29 14:51:21 +0900

    feat : 다크테마 구현중

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux/es/exports";
+
+import styled from "styled-components";
 
 const Main = () => {
+  const mode = useSelector((state) => state.theme.mode);
+
   return (
-    <div>
-      <span>test</span>
-    </div>
+    <MainContainer>
+      <ChangeButton>Change</ChangeButton>
+    </MainContainer>
   );
 };
 
 export default Main;
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ChangeButton = styled.button`
+  width: 50%;
+  height: 30%;
+  border: none;
+  border-radius: 100px;
+  background-color: aqua;
+  cursor: pointer;
+`;
